54c98a7065ca66a4bda0e8c13b24e23e69f76c8c
abhiisOOO <lakraabhinav72@gmail.com>
2026-08-06 15:50:53 +0530

    add protect middleware with jwt authentication.

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,24 @@
+import jwt from "jsonwebtoken";
+import { catchAsync } from "../utils/asyncHandler";
+import { AppError } from "../utils/appError";
+import { User } from "../models/userModel";
+
+export const verify = catchAsync(async (req, res, next) => {
+  const token = req.cookies.accessToken;
+
+  if (!token) {
+    return next(new AppError("No authentication token provided! ", 401));
+  }
+
+  const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+  const user = await User.findById(decoded._id);
+
+  if (!user) {
+    return next(new AppError("User no longer exists!", 401));
+  }
+
+  req.user = user;
+  console.log(user.name);
+  next();
+});
